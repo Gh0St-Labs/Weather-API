c83049d119ea892dfdcc87f26c0c13c0dffff25e
Gh0St_Labs <kordan.goat@gmail.com>
2023-04-01 08:59:47 +0530

    Added Code to return the Particular Temperature of the Date from the user's inputs.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 the_app = Flask("MyWeatherAPI")
 
@@ -8,7 +9,14 @@ def home():
 
 @the_app.route("/myweatherapi/v1/<enter_station_no_here>/<enter_date_here>")
 def about(enter_station_no_here, enter_date_here):
-    return render_template("about.html")
+    station_data_and_stuff = "data_small/TG_STAID" + str(enter_station_no_here).zfill(6) + ".txt"
+    data = pd.read_csv(station_data_and_stuff, skiprows=20, parse_dates=['    DATE'])
+    user_station = enter_station_no_here
+    user_date = enter_date_here
+    user_temperature = data.loc[data['    DATE'] == enter_date_here]['   TG'].squeeze() / 10
+    ut_fahrenheit = user_temperature * (9/5) + 32
+    return {"Your Station No:":user_station, "Date:":user_date, "Temperature:":user_temperature,
+            "Temperature in Fahrenheit":ut_fahrenheit}
 
 if __name__ == "__main__":
-    the_app.run(debug=True)
+    the_app.run(debug=True)      
